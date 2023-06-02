33afc5da1f755140b3c8165b7a1605370a374677
Glitch <glitch.txs@gmail.com>
2023-06-02 14:35:42 -0300

    config

--- example/src/pages/_app.tsx
+++ example/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { connectors, mainnet, w3init } from '@glitch-txs/w3/dist'
+import { connectors, mainnet, w3init } from '@glitch-txs/w3'
 
 w3init({connectors, chains:[mainnet]})
 
