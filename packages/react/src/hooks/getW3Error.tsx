8b1c6e6f92f28d75821d58e121f5e4962f1fb982
Glitch <glitch.txs@gmail.com>
2023-06-10 01:12:51 -0300

    error typo

--- packages/react/src/hooks/getW3Error.tsx
+++ packages/react/src/hooks/getW3Error.tsx
@@ -2,5 +2,5 @@ import { useSyncExternalStore } from 'react'
 import { web3Store } from '@glitch-txs/w3'
 
 export function getW3Error(){
-  return useSyncExternalStore((cb)=>web3Store.subscribe((state: any)=> state.errorMessage,cb),()=>web3Store.getState().errorMessage,()=>web3Store.getState().errorMessage)
+  return useSyncExternalStore((cb)=>web3Store.subscribe((state: any)=> state.error,cb),()=>web3Store.getState().error,()=>web3Store.getState().error)
 }
