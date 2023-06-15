235587a8932a02dbe8124efce0c391352fbe7c45
Glitch <glitch.txs@gmail.com>
2023-06-15 17:03:14 -0300

    update version example

--- example/src/pages/_app.tsx
+++ example/src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { W3, createW3Props, initWallets, mainnet } from 'w3-evm-react'
+import { W3, initW3, initWallets, mainnet } from 'w3-evm-react'
 import { walletIcons } from '../../utils/icons'
 
-const w3props = createW3Props({
+const w3props = initW3({
   wallets: initWallets(walletIcons),
   chains:[mainnet]
 })
