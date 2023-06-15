dc13b97ee6a06c565f50face29c3e338d5b6fc51
Glitch <glitch.txs@gmail.com>
2023-06-15 16:24:18 -0300

    typo

--- packages/react/src/props.ts
+++ packages/react/src/props.ts
@@ -2,7 +2,7 @@ import { initEIP6963, web3Store } from "w3-evm"
 import { KEY_WALLET } from "./utils/constants"
 import { W3Props, ReturnW3Props } from "./types"
 
-export function createW3props({wallets, chains, onboard = true, EIP6963 = true, hydration = true }: W3Props):ReturnW3Props{
+export function createW3Props({wallets, chains, onboard = true, EIP6963 = true, hydration = true }: W3Props):ReturnW3Props{
   web3Store.setState((state)=>({ onboard, chains, wallets }))
   // wallets are return to initialize after the DOM is painted
   if(typeof window === 'undefined' || hydration) return { EIP6963, hydration, wallets }
