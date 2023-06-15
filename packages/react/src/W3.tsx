45cf98b501b4dfa679b63a4c3c92ddb367db7a18
Glitch <glitch.txs@gmail.com>
2023-06-15 16:06:07 -0300

    fix: props function sync

--- packages/react/src/W3.tsx
+++ packages/react/src/W3.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { BaseWallet, initEIP6963 } from 'w3-evm'
+import { BaseWallet, initEIP6963, web3Store } from 'w3-evm'
 import { KEY_WALLET } from './utils/constants'
 
 let init = 0
@@ -8,11 +8,12 @@ export function W3({ EIP6963, cache, wallets }:{ EIP6963: boolean, cache: boolea
   useEffect(()=>{
     if(cache || !wallets) return
     if(init === 0){
+      if(!window.localStorage.getItem(KEY_WALLET))
+      web3Store.setState((state)=>({wait:{state: false, reason:'' }}))
+
       if(EIP6963) initEIP6963()
-      const connectedWallet = window.localStorage.getItem(KEY_WALLET)
-      if(!connectedWallet) return
-      for(let w of wallets)
-      if(w.id === connectedWallet) w.init()
+      
+      for(let w of wallets) w.init()
     }
     // This component must be mounted only once in the whole application's lifecycle
     return ()=>{init = 1}
--- packages/react/src/props.ts
+++ packages/react/src/props.ts
@@ -2,20 +2,18 @@ import { initEIP6963, web3Store } from "w3-evm"
 import { KEY_WALLET } from "./utils/constants"
 import { W3Props, ReturnW3Props } from "./types"
 
-export async function createW3props({wallets, chains, onboard = true, EIP6963 = true, cache = false }: W3Props):Promise<ReturnW3Props>{
+export function createW3props({wallets, chains, onboard = true, EIP6963 = true, cache = false }: W3Props):ReturnW3Props{
   web3Store.setState((state)=>({ onboard, chains, wallets }))
   // wallets are return to initialize after the DOM is painted
   if(typeof window === 'undefined' || !cache) return { EIP6963, cache, wallets }
+
   if(EIP6963) initEIP6963()
-  const connectedWallet = window.localStorage.getItem(KEY_WALLET)
-  if(!connectedWallet){
-    web3Store.setState((state)=>({wait:{state: false, reason:'' }}))
-    return { EIP6963, cache }
-  }
-  // injection delay - https://groups.google.com/a/chromium.org/g/chromium-extensions/c/ib-hi7hPdW8/m/34mFf8rrGQAJ?pli=1
-  await new Promise(r => setTimeout(r, 100))
-  for(let w of wallets)
-  if(w.id === connectedWallet) w.init()
+
+  if(!window.localStorage.getItem(KEY_WALLET))
+  web3Store.setState((state)=>({wait:{state: false, reason:'' }}))
+  
+  //even if they are not connected we set this.ready to true
+  for(let w of wallets) w.init()
   
   return { EIP6963, cache }
 }
