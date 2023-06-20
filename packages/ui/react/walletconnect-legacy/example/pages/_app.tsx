4f1200dfb0f356fd5e20507f8db0946cc7a86d3c
Glitch <glitch.txs@gmail.com>
2023-06-20 20:54:14 -0300

    update modal name

--- packages/ui/react/walletconnect-legacy/example/pages/_app.tsx
+++ packages/ui/react/walletconnect-legacy/example/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { W3, mainnet, initW3 } from 'w3-evm-react'
-import { W3UI, initModalWallets } from 'walletconnect-legacy-ui'
+import { W3Modal, initModalWallets } from 'walletconnect-legacy-ui'
 
 const w3props = initW3({
   wallets:initModalWallets(),
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <W3 {...w3props} />
-      <W3UI/>
+      <W3Modal/>
       <Component {...pageProps} />
     </>
   )
